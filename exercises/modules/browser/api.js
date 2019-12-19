815dd18e40c7035153f37a4ae79ea204409bf23e
mr-git-gud <git.pwn@pm.me>
2019-12-19 21:12:30 +0000

    Update start branch

--- exercises/modules/browser/api.js
+++ exercises/modules/browser/api.js
@@ -1,17 +1,25 @@
-window.App = window.App || {}
+const {
+  users,
+  posts
+} = require('./data');
 
-window.App.getUserById = (id, cb) => {
+const getUserById = (id, cb) => {
+  // simulate API call
+  setTimeout(() => {
+    const user = users.find(user => user.id === id);
+    cb(user);
+  }, 150);
+};
+
+const getPostsForUser = (userId, cb) => {
   // simulate API call
   window.setTimeout(() => {
-    const user = window.App.users.find(user => user.id === id)
-    cb(user)
-  }, 150)
-}
+    const post = posts.filter(post => post.createdBy === userId);
+    cb(post);
+  }, 150);
+};
 
-window.App.getPostsForUser = (userId, cb) => {
-  // simulate API call
-  window.setTimeout(() => {
-    const posts = window.App.posts.filter(post => post.createdBy === userId)
-    cb(posts)
-  }, 150)
+module.exports = {
+  getPostsForUser,
+  getUserById
 }
--- exercises/testing/test.js
+++ exercises/testing/test.js
@@ -1,4 +1,9 @@
 // write some tests
+const users = require('./users');
+
 describe('users', () => {
-  
+    test("look up user", () => {
+        let u = users.findUser(1)
+        expect(u).toBeTruthy()
+    })
 })
