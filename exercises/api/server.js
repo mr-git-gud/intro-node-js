b78e9f8598eb392f9390ef56b515340bfa32dccf
mr-git-gud <git.pwn@pm.me>
2019-12-20 21:55:50 +0000

    Check master again

--- exercises/api/server.js
+++ exercises/api/server.js
@@ -2,22 +2,38 @@ const http = require('http');
 const url = require('url');
 const fs = require('fs')
 const path = require('path')
+const mime = require('mime')
 
 /**
  * this function is blocking, fix that
  * @param {String} name full file name of asset in asset folder
  */
 const findAsset = (name) => {
-  const assetPath = path.resolve(__dirname, 'assets', name);
-  const file = fs.readFile(assetPath, (err, data) => {
-    if (err) console.log(err);
-    else return data.toString();
-  });
-  return file;
+  return new Promise((resolve, reject) => {
+    const assetPath = path.join(__dirname, 'assets', name);
+    fs.readFile(assetPath, (err, file) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(file);
+      }
+    })
+  })
 }
 
 const hostname = '127.0.0.1';
 const port = 3000;
+
+const router = {
+  "/ GET": {
+    asset: "text/html",
+    type: mime.type("html")
+  },
+  "/ styles.css": {
+    asset: "styles/css",
+    type: mime.type("css")
+  }
+}
 
 // log incoming request coming into the server. Helpful for debugging and tracking
 const logRequest = (method, route, status) => console.log(method, route, status)
@@ -27,21 +43,16 @@ const server = http.createServer((req, res) => {
   const route = url.parse(req.url).pathname
   // this is sloppy, especially with more assets, create a "router
 
-  if (route === "/") {
-    res.writeHead(200, {
-      'Content-Type': 'text/html'
-    });
-    res.write(findAsset('index.html'));
-    logRequest(method, route, 200);
-    res.end();
-  } else {
-    res.writeHead(404, {
-      'Content-Type': 'text/html'
-    });
-    res.write("Oops we couldn't find that!");
-    res.end();
-  }
-  res.send;
+  const routerMatch = router[`${route} ${method}`]
+
+  const {
+    asset,
+    type
+  } = routerMatch;
+
+  res.writeHead(200, type);
+  res.write(findAsset('index.html'));
+  res.end();
 })
 
 server.listen(port, hostname, () => {
